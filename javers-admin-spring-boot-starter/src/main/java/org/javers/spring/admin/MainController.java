f7eb5b115ad4881cb41fce25653e3f009f9e0b3d
Bartosz Walacik <bartosz.walacik@allegrogroup.com>
2017-10-24 13:28:01 +0200

    mocked entities

--- javers-admin-spring-boot-starter/src/main/java/org/javers/spring/admin/MainController.java
+++ javers-admin-spring-boot-starter/src/main/java/org/javers/spring/admin/MainController.java
@@ -1,9 +1,13 @@
 package org.javers.spring.admin;
 
 import org.javers.common.collections.Lists;
+import org.javers.common.reflection.ReflectionUtil;
 import org.javers.core.Javers;
+import org.javers.core.metamodel.object.CdoSnapshot;
+import org.javers.repository.jql.QueryBuilder;
 import org.javers.spring.admin.model.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,12 +27,16 @@ public class MainController {
     @RequestMapping("/entities")
     public List<Entity> entities() {
         return Lists.asList(
-                new Entity("org.my.company.Person", 10, 200),
-                new Entity("org.my.company.Hierarchy", 1, 5),
-                new Entity("org.my.company.Route", 12, 50),
-                new Entity("org.my.company.Speaker", 10, 50),
-                new Entity("org.my.company.Attendee", 10, 50),
-                new Entity("org.my.company.Conference", 10, 50)
+                new Entity("org.javers.organization.structure.domain.Employee", 10, 200),
+                new Entity("org.javers.organization.structure.domain.Hierarchy", 2, 10),
+                new Entity("org.javers.organization.structure.domain.Person", 12, 100)
         );
+    }
+
+    @RequestMapping("/entity/{entityName}/")
+    public String latestSnapshots(@PathVariable String entityName) {
+        List<CdoSnapshot> snapshots = javers.findSnapshots(
+                QueryBuilder.byClass(ReflectionUtil.classForName(entityName)).build());
+        return javers.getJsonConverter().toJson(snapshots);
     }
 }
