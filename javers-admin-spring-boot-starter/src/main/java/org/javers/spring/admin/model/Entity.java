5bee279cdc6d783b8e79f04d6eae2a8567002951
Bartosz Walacik <bartosz.walacik@allegrogroup.com>
2017-10-24 17:28:26 +0200

    Entity change

--- javers-admin-spring-boot-starter/src/main/java/org/javers/spring/admin/model/Entity.java
+++ javers-admin-spring-boot-starter/src/main/java/org/javers/spring/admin/model/Entity.java
@@ -1,12 +1,16 @@
 package org.javers.spring.admin.model;
 
 public class Entity {
+    private final String id;
     private final String name;
     private final int count;
     private final int snapshots;
 
-    public Entity(String name, int count, int snapshots) {
-        this.name = name;
+
+    public Entity(String id, int count, int snapshots) {
+        this.id = id;
+        String[] split = id.split("\\.");
+        this.name = split[split.length - 1];
         this.count = count;
         this.snapshots = snapshots;
     }
@@ -21,5 +25,9 @@ public class Entity {
 
     public int getSnapshots() {
         return snapshots;
+    }
+
+    public String getId() {
+        return id;
     }
 }
